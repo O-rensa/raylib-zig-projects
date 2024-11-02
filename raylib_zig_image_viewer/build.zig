55b3bce90092322eb0307b213bd8b3a9788d1cdf
Renz Lendio <131181504+O-rensa@users.noreply.github.com>
2024-11-02 22:31:17 +0800

    Added drop file feature

--- raylib_zig_image_viewer/build.zig
+++ raylib_zig_image_viewer/build.zig
@@ -1,8 +1,5 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
 
@@ -17,6 +14,8 @@ pub fn build(b: *std.Build) void {
     const raylib = raylib_dep.module("raylib"); // main raylib module;
     const raygui = raylib_dep.module("raygui"); // raygui module;
     const raylib_artifact = raylib_dep.artifact("raylib");
+
+    raylib_artifact.defineCMacro("SUPPORT_FILEFORMAT_JPG", null); // add support for JPEG
 
     const exe = b.addExecutable(.{
         .name = "raylib_zig_image_viewer",
--- raylib_zig_image_viewer/src/main.zig
+++ raylib_zig_image_viewer/src/main.zig
@@ -1,21 +1,64 @@
 const std = @import("std");
-const rlz = @import("raylib");
+const rl = @import("raylib");
+
+const Texture2dArrayList = std.ArrayList(rl.Texture2D);
 
 pub fn main() !void {
     const screenWidth = 800;
     const screenHeight = 450;
 
-    rlz.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
-    defer rlz.closeWindow(); // Close window and OpenGL context
+    rl.setConfigFlags(rl.ConfigFlags{ .window_resizable = true, .vsync_hint = true });
+    rl.initWindow(screenWidth, screenHeight, "Raylib Image Viewer");
+    defer rl.closeWindow(); // Close window and OpenGL context
+    rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
 
-    rlz.setTargetFPS(60); // Set our game to run at 60 frames-per-second
+    // arena allocator
+    var arena = std.heap.ArenaAllocator.init(std.heap.c_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
 
-    while (!rlz.windowShouldClose()) { // Detect window close button or ESC key
-        rlz.beginDrawing();
-        defer rlz.endDrawing();
+    var textures = Texture2dArrayList.init(allocator);
+    defer {
+        for (textures.items) |texture| {
+            rl.unloadTexture(texture);
+        }
 
-        rlz.clearBackground(rlz.Color.white);
+        textures.deinit();
+    }
 
-        rlz.drawText("Congrats! You created your first window!", 190, 200, 20, rlz.Color.light_gray);
+    while (!rl.windowShouldClose()) { // Detect window close button or ESC key
+        if (rl.isFileDropped()) {
+            const dropped_files = rl.loadDroppedFiles();
+            defer rl.unloadDroppedFiles(dropped_files);
+            // convert to slice
+            const dropped_files_slice = dropped_files.paths[0..dropped_files.count];
+            for (dropped_files_slice) |dropped_file_path| {
+                var texture = rl.loadTexture(dropped_file_path);
+                if (0 == texture.id) continue;
+
+                // generate mipmaps
+                rl.genTextureMipmaps(&texture);
+                if (1 == texture.mipmaps) { // check mipmaps
+                    std.debug.print("{s}", .{"Mipmaps failed to generate! \n"});
+                }
+
+                // set texture filter
+                rl.setTextureFilter(texture, rl.TextureFilter.texture_filter_trilinear);
+                try textures.append(texture);
+            }
+        }
+
+        { // drawing
+            rl.beginDrawing();
+            defer rl.endDrawing();
+            rl.clearBackground(rl.Color.white); // background color
+
+            var x: i32 = 0;
+
+            for (textures.items) |texture| {
+                rl.drawTextureEx(texture, rl.Vector2{ .x = @floatFromInt(x), .y = 0 }, 0, 0.3, rl.Color.white);
+                x += texture.width;
+            }
+        }
     }
 }
