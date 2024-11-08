d1b07e08c523180060394ec318eb6cd46a2781b3
Renz Lendio <131181504+O-rensa@users.noreply.github.com>
2024-11-08 23:08:56 +0800

    Changed arraylist to deque

--- snake_game/src/snake.zig
+++ snake_game/src/snake.zig
@@ -1,28 +1,29 @@
 const std = @import("std");
 const rl = @import("raylib");
 const dc = @import("define_const.zig");
+const dq = @import("deque.zig");
 
 const cell_size = dc.CELLSIZE;
 const dark_green = dc.DARK_GREEN;
 const Snake = @This();
 const allocator = std.heap.page_allocator;
-const ArrayList = std.ArrayList(rl.Vector2);
+const Deque = dq.Deque(rl.Vector2);
 // fields
-body: ArrayList,
+body: Deque,
 
 pub fn init() !Snake {
     var s = Snake{
-        .body = ArrayList.init(allocator),
+        .body = try Deque.init(allocator),
     };
-    try s.body.append(rl.Vector2{
+    try s.body.pushBack(rl.Vector2{
         .x = 6,
         .y = 9,
     });
-    try s.body.append(rl.Vector2{
+    try s.body.pushBack(rl.Vector2{
         .x = 5,
         .y = 9,
     });
-    try s.body.append(rl.Vector2{
+    try s.body.pushBack(rl.Vector2{
         .x = 4,
         .y = 9,
     });
@@ -34,9 +35,9 @@ pub fn deinit(self: Snake) void {
 }
 
 pub fn draw(self: Snake) void {
-    for (self.body.items) |item| {
-        const pos_x: i32 = @intFromFloat(item.x);
-        const pos_y: i32 = @intFromFloat(item.y);
+    for (0..self.body.len()) |idx| {
+        const pos_x: i32 = @intFromFloat(self.body.get(idx).?.x);
+        const pos_y: i32 = @intFromFloat(self.body.get(idx).?.y);
         rl.drawRectangle(pos_x * cell_size, pos_y * cell_size, cell_size, cell_size, dark_green);
     }
 }
