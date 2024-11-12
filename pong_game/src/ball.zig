1472e42a3a9487308fdd6af294b0a5f960203363
Renz Lendio <131181504+O-rensa@users.noreply.github.com>
2024-11-12 23:01:56 +0800

    Ball now Moves

---
+++ pong_game/src/ball.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+const rl = @import("raylib");
+
+const Ball = @This();
+x: f32,
+y: f32,
+speed_x: i32,
+speed_y: i32,
+radius: i32,
+
+pub fn init(radius: i32, x_pos: f32, y_pos: f32, speed_x: i32, speed_y: i32) Ball {
+    return Ball{
+        .radius = radius,
+        .x = x_pos,
+        .y = y_pos,
+        .speed_x = speed_x,
+        .speed_y = speed_y,
+    };
+}
+
+pub fn draw(self: Ball) void {
+    const x: i32 = @intFromFloat(self.x);
+    const y: i32 = @intFromFloat(self.y);
+    const radius: f32 = @floatFromInt(self.radius);
+    rl.drawCircle(x, y, radius, rl.Color.white);
+}
+
+pub fn update(self: *Ball) void {
+    self.*.y += @as(f32, @floatFromInt(self.*.speed_y));
+    self.*.x += @as(f32, @floatFromInt(self.speed_x));
+}
--- pong_game/src/main.zig
+++ pong_game/src/main.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const rl = @import("raylib");
 
+const Ball = @import("ball.zig");
+
 pub fn main() !void {
     const screen_width = 1280;
     const screen_height = 800;
@@ -9,16 +11,27 @@ pub fn main() !void {
     defer rl.closeWindow(); // Close window and OpenGL context
     rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
 
+    // ball
+    var ball = Ball.init(20, screen_width / 2, screen_height / 2, 7, 7);
+
     // Main game loop
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
         rl.beginDrawing();
         defer rl.endDrawing();
 
+        // update
+        {
+            ball.update();
+        }
+
         // drawing
         {
+            // clear background
+            rl.clearBackground(rl.Color.black);
+
             rl.drawLine(screen_width / 2, 0, screen_width / 2, screen_height, rl.Color.white);
             // draw circle on the middle of the screen;
-            rl.drawCircle(screen_width / 2, screen_height / 2, 20, rl.Color.white);
+            ball.draw();
 
             rl.drawRectangle(10, screen_height / 2 - 60, 25, 120, rl.Color.white);
             rl.drawRectangle(screen_width - 35, screen_height / 2 - 60, 25, 120, rl.Color.white);
